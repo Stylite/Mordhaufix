09ae82d728b61aa522121fc97e32effc969ebb45
William <me@stylite.me>
2024-01-27 00:56:06 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,8 +27,8 @@ TZ=${TZ:-UTC}
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
-export INTERNAL_IP
+#INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+#export INTERNAL_IP
 
 # Default the IMAGE_PROMPT environment variable to something nice
 IMAGE_PROMPT=${IMAGE_PROMPT:-$'\033[1m\033[33mcontainer@pterodactyl~ \033[0m'}
